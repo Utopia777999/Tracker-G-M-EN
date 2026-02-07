5b04bc3ccede05ef0818c0429cd4fb78a4b01bf1
L7 <araenxu@163.com>
2026-02-07 20:52:04 +0800

    Updates

--- assets/main-CbrJDMcv.js
+++
@@ -1,1 +0,0 @@
-import{T as r}from"./index-BJfcsCWt.js";new r({appId:"xhs-portal-001"});
--- assets/mall-BG9ikxR8.js
+++
@@ -1,1 +0,0 @@
-import{T as r}from"./index-BJfcsCWt.js";new r({appId:"mall-portal-001",traceSelectors:[".goods-card"]});
