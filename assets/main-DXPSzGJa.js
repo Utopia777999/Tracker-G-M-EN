7603350212d08064c84e8fb58bdd4376f9a6fdb2
L7 <araenxu@163.com>
2026-02-09 20:15:31 +0800

    Updates

--- assets/main-DXPSzGJa.js
+++
@@ -1,1 +0,0 @@
-import{T as a}from"./index-3ToTZjbk.js";new a({appId:"simulated-portal-001"});
--- assets/mall-DWXA620h.js
+++
@@ -1,1 +0,0 @@
-import{T as a}from"./index-3ToTZjbk.js";new a({appId:"simulated-mall-portal-001",traceSelectors:[".goods-card"]});
